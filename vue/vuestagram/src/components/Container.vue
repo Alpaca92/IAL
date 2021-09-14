7c4f3442f38237d94a101f8fc2fc53cb644a1611
alpaca <zxcvbnm5288@gmail.com>
2021-09-14 23:29:39 +0900

    recap v-for, props, data binding, and components

---
+++ vue/vuestagram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post :postDatas="postDatas" />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+  name: "Container",
+  components: {
+    Post,
+  },
+  props: {
+    postDatas: Array,
+  }
+};
+</script>
+
+<style></style>
