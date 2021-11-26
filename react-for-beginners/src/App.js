b693e8199d29e753bf0adf38667dbbc9d9092bd8
alpaca <zxcvbnm5288@gmail.com>
2021-11-26 11:01:39 +0900

    create abb.module.css

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,15 @@
 import Button from "./Button";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => console.log(prev));
+
   return (
     <>
-      <h1>hello !</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick= text={"click me"} />
     </>
   );
 }
