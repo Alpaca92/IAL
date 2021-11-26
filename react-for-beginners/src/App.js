490a68fe78c90575e54609ede7474ad7142cd654
alpaca <zxcvbnm5288@gmail.com>
2021-11-26 11:29:22 +0900

    learn useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,15 +1,19 @@
-import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((prev) => console.log(prev));
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
 
   return (
     <>
       <h1 className={styles.title}>{counter}</h1>
-      <Button onClick= text={"click me"} />
+      <button onClick={onClick}>click me</button>
     </>
   );
 }
