309b2f6b4eac11f64909a98780ab214949893f17
alpaca <zxcvbnm5288@gmail.com>
2021-11-26 13:34:33 +0900

    clean up function

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,21 +1,24 @@
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+
+    return () => console.log("destroyed :(");
+  }, []);
+
+  return <h1>Hello !</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setShowing((prev) => !prev);
 
-  useEffect(() => console.log("i run only once."), []);
-  useEffect(() => console.log(`i run when 'keyword' changes.`), [keyword]);
-  useEffect(() => console.log(`i run when 'counter' changes.`), [counter]);
   return (
     <>
-      <input onChange={onChange} type="text" placeholder="Search here" />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </>
   );
 }
