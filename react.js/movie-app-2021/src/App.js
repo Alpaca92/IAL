884a197cae3383b9e7e484ac4ee398b5c68fab0e
alpaca <zxcvbnm5288@gmail.com>
2021-12-02 13:38:37 +0900

    use async/await instead then

---
+++ react.js/movie-app-2021/src/App.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?limit=50&sort_by=year&minimum_rating=8.5`
+    );
+    const json = await response.json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <>{loading ? <h1>Loading...</h1> : null}</>;
+}
+
+export default App;
