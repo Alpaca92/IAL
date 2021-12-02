c01cb8bf3e3ced5e2a037bc04822af2600dcc658
alpaca <zxcvbnm5288@gmail.com>
2021-12-02 13:08:33 +0900

    update coin tracker

--- react.js/coin-tracker/src/App.js
+++ react.js/coin-tracker/src/App.js
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <></>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <strong>Loading...</strong>;
+
+  return (
+    <>
+      <h1>The Coins !</h1>
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
