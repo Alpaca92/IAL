628cdf3f0db0f6ffaceb4c84d187b06ed4ed7aa6
alpaca <zxcvbnm5288@gmail.com>
2022-02-14 18:20:03 +0900

    #1.2: create ws on http server

--- javascript/noom/src/server.js
+++ javascript/noom/src/server.js
@@ -1,5 +1,17 @@
 import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
 
-app.listen(5000, () => console.log("✔ url: http://localhost/5000"));
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+
+server.listen(5000, () => console.log("✔ URL: http://localhost:5000"));
