f2458de36912a3d3e55adf26a865c23e5717183e
alpaca <zxcvbnm5288@gmail.com>
2022-02-15 20:15:07 +0900

    #7.16: shared layout part.1

--- react.js/framer/src/App.tsx
+++ react.js/framer/src/App.tsx
@@ -11,72 +11,69 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50%;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
-  background-color: rgba(200, 200, 200, 1);
+  height: 100px;
   border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-  /* position: absolute; */
-  top: 100px;
+  background-color: rgba(200, 200, 200, 1);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.2,
-    },
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlayVariants = {
+  invisible: {
+    backgroundColor: "rgba(0,0,0,0)",
   },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
+  visible: {
+    backgroundColor: "rgba(0,0,0,0.65)",
+  },
 };
 
 function App() {
-  const [back, setBack] = useState(false);
-  const [visible, setVisible] = useState(1);
-  const nextBtn = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 1 : ++prev));
-  };
-  const prevBtn = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 10 : --prev));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggle = () => setClicked((prev) => !prev);
 
   return (
-    <Wrapper>
-      <AnimatePresence exitBeforeEnter custom={back}>
-        <Box
-          custom={back}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
+    <Wrapper onClick={toggle}>
+      <Grid>
+        <Box layoutId="box" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked ? (
+          <Overlay
+            variants={overlayVariants}
+            initial="invisible"
+            animate="visible"
+            exit="invisible"
+          >
+            <Box
+              layoutId="box"
+              style={{ width: 400, height: 200, backgroundColor: "#fff" }}
+            />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-      <button onClick={nextBtn}>next</button>
-      <button onClick={prevBtn}>prev</button>
     </Wrapper>
   );
 }
