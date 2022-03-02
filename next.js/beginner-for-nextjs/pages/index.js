e454455fe6eee2ea3826b0dca7b4333cc294ccb4
alpaca <zxcvbnm5288@gmail.com>
2022-03-02 11:50:30 +0900

    #2.1: fetching data

--- next.js/beginner-for-nextjs/pages/index.js
+++ next.js/beginner-for-nextjs/pages/index.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "2fb9b79ab51f1e3d6820fdd5feda6a3e";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
