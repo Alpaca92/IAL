64a4c7096d82e47ea050c280b6e64bc18589f90b
alpaca <zxcvbnm5288@gmail.com>
2022-03-04 04:13:34 +0900

    tailwind setup

---
+++ next.js/carrot-clone/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    // 해당 경로에서 tailwind가 사용된다는 것을 명시해 줍니다
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
