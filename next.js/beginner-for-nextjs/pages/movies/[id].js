408f8a70a2ffb3882347ae06550c03b67656348e
alpaca <zxcvbnm5288@gmail.com>
2022-03-03 14:27:09 +0900

    #2.8: catch all

--- next.js/beginner-for-nextjs/pages/movies/[id].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Detail() {
-  const {
-    query: { id, title },
-  } = useRouter();
-
-  return <div>{`id: ${id}, title: ${title}`}</div>;
-}
