26f1fbef37fc25a6439e9ed9559faec6cc279fc3
ayaan <be.minimal.o.o9@gmail.com>
2022-04-12 17:51:31 +0900

    #8.4: with handler

--- next.js/carrot-clone/libs/client/useMutation.tsx
+++ next.js/carrot-clone/libs/client/useMutation.tsx
@@ -9,6 +9,11 @@ interface UseMutationState {
 export default function useMutation(
   url: string
 ): [(data: any) => void, UseMutationState] {
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
