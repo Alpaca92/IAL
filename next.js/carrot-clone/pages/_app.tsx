72413bf46294fbbd4641c59a166c2bf03f6570b0
ayaan <be.minimal.o.o9@gmail.com>
2022-04-18 16:52:42 +0900

    #10.4 useUser Refactor

--- next.js/carrot-clone/pages/_app.tsx
+++ next.js/carrot-clone/pages/_app.tsx
@@ -1,8 +1,17 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <div className="mx-auto w-full max-w-xl">
+        <Component {...pageProps} />;
+      </div>
+    </SWRConfig>
+  );
 }
 
 export default MyApp;
