145898812de56b49dcacc1d8b25a47b00c72650a
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2022-11-07 09:20:56 +0900

    firebase initialization

---
+++ react.js/nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkXVGG6nINM1-VrougxtpksGSTwRNis3A",
+  authDomain: "nwitter-ca194.firebaseapp.com",
+  projectId: "nwitter-ca194",
+  storageBucket: "nwitter-ca194.appspot.com",
+  messagingSenderId: "922767916897",
+  appId: "1:922767916897:web:714b24652f26e1b0f28f20",
+};
+
+export default initializeApp(firebaseConfig);
