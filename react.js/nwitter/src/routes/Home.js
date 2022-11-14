e0f6e2f0371b840dac712182323e85da92a38486
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2022-11-14 18:53:56 +0900

    connect db

--- react.js/nwitter/src/routes/Home.js
+++ react.js/nwitter/src/routes/Home.js
@@ -1,3 +1,43 @@
-const Home = () => <span>Home</span>;
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "firebaseInstance";
+import { useState } from "react";
+
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const collectionRef = collection(db, "nweets");
+    const docRef = await addDoc(collectionRef, {
+      nweet,
+      createdAt: Date.now(),
+    });
+
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={nweet}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
