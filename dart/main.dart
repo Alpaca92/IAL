09e3fdbe3787cbbb0a810e5b0f2bee7f2b4189c4
Ayaan(macbook pro) <zxcvbnm5288@gmail.com>
2022-12-23 01:52:22 +0900

    #1.6 Constant Variables

--- dart/main.dart
+++ dart/main.dart
@@ -1,13 +1,13 @@
-void main() {
-  // late는 var(or final)앞에 붙여줄 수 있음
-  // 데이터가 없는 변수를 만들어주고 나중에 데이터를 assign할 수 있도록 해줌
-  late final String name;
-  print(name); // error
-  name = 'ayaan';
-  print(name);
+import 'dart:html';
 
-  final String anotherName;
-  print(anotherName); // error
-  anotherName = 'ayaan';
-  print(anotherName);
+void main() {
+  // const는 js, ts와는 다르다
+  // const는 final과 유사한데, compile-time constant를 생성해준다
+  const API_KEY = '123123124012849012';
+
+  // const는 컴파일할 때 알고있는 값을 사용해야 한다
+  // 예를 들어 API로부터 받아오거나 하는 값들은 컴파일할 때 알 수 없는 값들이므로
+  // 이 경우에는 final이나 var를 사용해야 한다
+  const SOME_API = fetchApi(); // non-compile-time constant
+  final SOME_API_2 = fetchApi();
 }
