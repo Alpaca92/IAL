b280ffb388da7d3c953733e0983240f497194442
Ayaan(macbook pro) <zxcvbnm5288@gmail.com>
2022-12-23 01:42:19 +0900

    #1.5 Late Variables

--- dart/main.dart
+++ dart/main.dart
@@ -1,6 +1,13 @@
 void main() {
-  // final like const on js(or ts)
-  final name = 'ayaan';
-  // or
-  final String anotherName = 'ayaan';
+  // late는 var(or final)앞에 붙여줄 수 있음
+  // 데이터가 없는 변수를 만들어주고 나중에 데이터를 assign할 수 있도록 해줌
+  late final String name;
+  print(name); // error
+  name = 'ayaan';
+  print(name);
+
+  final String anotherName;
+  print(anotherName); // error
+  anotherName = 'ayaan';
+  print(anotherName);
 }
