8e7b56583bdcc41fe0da9e2174d3b0ad300aa938
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2022-12-26 10:14:32 +0900

    #1.7 recap

--- dart/main.dart
+++ dart/main.dart
@@ -1,13 +1,27 @@
-import 'dart:html';
-
 void main() {
-  // const는 js, ts와는 다르다
-  // const는 final과 유사한데, compile-time constant를 생성해준다
-  const API_KEY = '123123124012849012';
+  // variable declaration
+  // 1. var - recommended
+  var name = 'bob';
+  // 2. explicitly declare - class properties를 작성할 때 사용
+  int number = 1;
+  // 3. final - can not change(reassign)
+  final age = 31;
+  // 4. dynamic - 어떤 데이터가 들어올지 모를 때 사용
+  dynamic location;
+  if (location is String) {
+    // do soth
+  }
+  // 5. const - compile 단계에서 알 수 있는 값들일 때 사용(compile-time constant)
+  const api_key = 'some_api_key';
 
-  // const는 컴파일할 때 알고있는 값을 사용해야 한다
-  // 예를 들어 API로부터 받아오거나 하는 값들은 컴파일할 때 알 수 없는 값들이므로
-  // 이 경우에는 final이나 var를 사용해야 한다
-  const SOME_API = fetchApi(); // non-compile-time constant
-  final SOME_API_2 = fetchApi();
+  // null safety
+  String? anotherName = 'john';
+  anotherName = null;
+  anotherName?.isEmpty;
+
+  // late - final, var, explicitly declare 앞에 붙는 수식어
+  late String some_data;
+  print(some_data); // error is occured
+  some_data = 'db';
+  print(some_data); // error don't occured
 }
