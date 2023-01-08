c2a5ce5d2ce778442735ed68035992dbcd1a4451
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2023-01-09 08:22:42 +0900

    #2.4 Maps

--- dart/main.dart
+++ dart/main.dart
@@ -1,16 +1,14 @@
-void main() {\
-  var oldFriends = [
-    'nico',
-    'lynn',
-  ];
-
-  // collection for
-  var newFriends = [
-    'lewis',
-    'ralph',
-    'darren',
-    for (var friend in oldFriends) '$friend :)',
-  ];
-
-  print(newFriends);
+void main() {
+  var player = {
+    // ts any == dart object
+    'nico': 'nico',
+    'xp': 19.99,
+    'superpower': false,
+  };
+  
+  Map<int, bool> anotherPlayer = {
+    1: false,
+    2: true,
+    4: true,
+  };
 }
