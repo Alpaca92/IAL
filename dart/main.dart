9f5502f325dda5d1cc43a850628c5cc5165193fc
Ayaan(macbook pro) <zxcvbnm5288@gmail.com>
2023-01-08 21:30:58 +0900

    #2.2 String Interpolation

--- dart/main.dart
+++ dart/main.dart
@@ -1,27 +1,9 @@
 void main() {
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    5,
-  ];
-  List<int> anotherNumbers = [
-    1,
-    2,
-    3,
-    4,
-  ];
-  numbers.add("1"); // error is occurred
-  numbers.add(1);
+  var name = 'ayaan';
+  var age = 30;
 
-  // collection if
-  var giveMeFive = true;
-  var collectionIf = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeFive) 5, // if giveMeFive is true, which return 5
-  ];
+  // string interpolation
+  var greeting = 'hello everyone, my name is $name and I am ${age + 1}';
+
+  print(greeting);
 }
