104fb6c77e86fad9b2e3bc3550970996dc1d9ec2
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2023-01-09 13:56:10 +0900

    #3.1 Named Parameters

--- dart/main.dart
+++ dart/main.dart
@@ -1,9 +1,16 @@
-String sayHello(String name) => "Hello $name ;)";
+String sayHello(String name, int age, String country) {
+  return "Hello $name, you are $age old, and you come from $country";
+}
 
-String anotherSayHello(String name) {
-  return "Hello $name ;)";
+// named parameters
+String anotherSayHello(
+    {required String name, required int age, required String country}) {
+  return "Hello $name, you are $age old, and you come from $country";
 }
 
 void main() {
-  print(sayHello('ayaan'));
+  print(sayHello('ayaan', 30, 'Korea'));
+
+  // named parameters
+  print(anotherSayHello(name: 'ayaan', age: 20, country: 'South Korea'));
 }
