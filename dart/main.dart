1c0867dfaa103a4899ff37c2812a1370995b557f
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2023-01-10 09:32:53 +0900

    #3.3 Optional Positional Parameters

--- dart/main.dart
+++ dart/main.dart
@@ -1,16 +1,11 @@
-String sayHello(String name, int age, String country) {
-  return "Hello $name, you are $age old, and you come from $country";
-}
-
-// named parameters
-String anotherSayHello(
-    {required String name, required int age, required String country}) {
-  return "Hello $name, you are $age old, and you come from $country";
-}
+String sayHello(
+  String name,
+  int age, [
+  String? country, // null로 출력되는 걸 원치 않는다면 default value를 넣을 수 있음
+]) =>
+    'Hello $name, $age years old, you are come from $country';
 
 void main() {
-  print(sayHello('ayaan', 30, 'Korea'));
-
-  // named parameters
-  print(anotherSayHello(name: 'ayaan', age: 20, country: 'South Korea'));
+  print(sayHello('ayaan', 13, 'suwon'));
+  print(sayHello('ayaan', 18));
 }
