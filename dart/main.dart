c2f39e0d55fb910610a3229f2c3293418fb18357
Ayaan(office mac) <zxcvbnm5288@gmail.com>
2023-01-19 12:59:22 +0900

    #4.4 Recap

--- dart/main.dart
+++ dart/main.dart
@@ -1,48 +1,39 @@
 class Player {
   final name;
-  int xp, age;
+  int xp;
   String team;
 
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age,
-  });
-
-  // name parameter의 경우 required를 해줘야 함
-  Player.createBluePlayer({
-    required String name,
-    required int age,
-  })  : this.age = age,
-        this.name = name,
-        this.team = 'blue',
-        this.xp = 0;
-
-  // positional parameter의 경우 default가 required임
-  Player.createRedPlayer(
-    String name,
-    int age,
-  )   : this.age = age,
-        this.name = name,
-        this.team = 'red',
-        this.xp = 0;
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
 
   void sayHello() {
-    print("Hello, my name is $name, team: $team, age: $age, xp: $xp");
+    print("Hello, my name is $name");
   }
 }
 
 void main() {
-  var player = Player.createBluePlayer(
-    name: "ayaan",
-    age: 21,
-  );
-  player.sayHello();
+  var apiData = [
+    {
+      "name": "ayaan",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lynn",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "darl",
+      "team": "red",
+      "xp": 0,
+    }
+  ];
 
-  var anotherPlayer = Player.createRedPlayer(
-    'lalalal',
-    13,
-  );
-  anotherPlayer.sayHello();
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
