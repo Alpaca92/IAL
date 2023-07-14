c8e3935b94b0cb9e4ac158119f0de09fcf4aa8cb
ayaan <ayaan@t-win.kr>
2023-07-14 11:00:42 +0900

    react native #3.3

--- react.native/workHardTravelHardApp/App.js
+++ react.native/workHardTravelHardApp/App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { theme } from "./colors";
+import { useState } from "react";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const work = () => setWorking(true);
+  const travel = () => setWorking(false);
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? "#fff" : theme.grey }}>Work</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? "#fff" : theme.grey }}>Travel</Text>
         </TouchableOpacity>
       </View>
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        style={styles.input}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+      />
     </View>
   );
 }
@@ -31,7 +44,14 @@ const styles = StyleSheet.create({
   },
   btnText: {
     fontSize: 38,
-    color: "#fff",
     fontWeight: 600,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
