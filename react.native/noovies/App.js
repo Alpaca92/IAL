430ec581ef23117636bc427529b2b5fb0b73107a
ayaan <ayaan@t-win.kr>
2023-07-28 16:13:17 +0900

    react.native master #2.3: use hooks

--- react.native/noovies/App.js
+++ react.native/noovies/App.js
@@ -1,50 +1,23 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
-import { Asset } from 'expo-asset';
-import { Image, Text, View } from 'react-native';
-
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === 'string') {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
+import { useAssets } from 'expo-asset';
+import { Text, View } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const fonts = loadFonts([Ionicons.font]);
-        const images = loadImages([
-          require('./assets/snack-icon.png'),
-          'https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg',
-        ]);
-
-        await Promise.all([...fonts, ...images]);
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setReady(true);
-      }
-    })();
-  }, []);
+  const [assets] = useAssets([require('./assets/snack-icon.png')]);
+  const [loaded] = Font.useFonts(Ionicons.font);
 
   const onLayoutRootView = useCallback(async () => {
-    if (ready) {
+    if (assets && loaded) {
       await SplashScreen.hideAsync();
     }
-  }, [ready]);
+  }, [assets, loaded]);
 
-  return ready ? (
+  return assets || loaded ? (
     <View onLayout={onLayoutRootView}>
       <Text>we are down loading !</Text>
     </View>
