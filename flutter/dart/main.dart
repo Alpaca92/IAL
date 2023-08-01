39c24ac7336cde6a97114c4f8071756721286459
ayaan <ayaan@t-win.kr>
2023-08-01 16:55:59 +0900

    dart #1.6

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,10 +1,4 @@
 void main() {
-  // late
-  late final String name;
-
-  // do something, go to api
-  name = 'test';
-
-  // 아래와의 차이점은 추후 class에서 알게 됨
-  final String name2;
+  // const: create compile time constants
+  const API_KEY = "some-api-key";
 }
