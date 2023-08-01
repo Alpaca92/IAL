573001021407223a7997bf764974be4e76207f37
ayaan <ayaan@t-win.kr>
2023-08-01 16:44:29 +0900

    dart #1.4

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,13 +1,4 @@
 void main() {
-  /**
-   * null safety
-   * 개발자가 null을 참조할 수 없음
-   * 따라서 null이 될 수 있는지 아는지를 정확하게 정해줘야 함
-   */
-
-  String name = 'test';
-  name = null; // error is occurred
-
-  String? name2 = 'test2';
-  name2 = null;
+  final name = 'name'; // == const in javascript
+  name = 'test';
 }
