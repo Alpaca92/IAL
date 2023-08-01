17743f82805137ad8a69387325f647a1d0b2c14d
ayaan <ayaan@t-win.kr>
2023-08-01 16:41:39 +0900

    dart #1.3

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,7 +1,13 @@
 void main() {
-  // dynamic type
-  var name;
-  name = 'name';
-  name = 12;
-  name = true;
+  /**
+   * null safety
+   * 개발자가 null을 참조할 수 없음
+   * 따라서 null이 될 수 있는지 아는지를 정확하게 정해줘야 함
+   */
+
+  String name = 'test';
+  name = null; // error is occurred
+
+  String? name2 = 'test2';
+  name2 = null;
 }
