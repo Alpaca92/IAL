a21a7da44678409afc5a582f35581b770caaf95b
ayaan <ayaan@t-win.kr>
2023-08-02 10:44:31 +0900

    dart #3.4

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,11 +1,7 @@
-// optional positional params
-// square brackets([])에 감싸지는 params는 반드시 마지막에 위치해야 함
-// square brackets을 쓰면 그 뒤의 모든 params도 optional이어야 함
-String sayHello(String name, [String country = 'cuba', int age = 10]) {
-  return "Hello $name, $country is very beautiful ! and you are $age";
-}
+// null aware operator (QQ operator)
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'anonymous';
 
 void main() {
-  print(sayHello('nico', 'seoul'));
-  print(sayHello('nico'));
+  capitalizeName('susan');
+  capitalizeName(null);
 }
