122b51e46d5ba5bc909816f1159759ac3d3024cf
ayaan <ayaan@t-win.kr>
2023-08-02 14:16:40 +0900

    dart #4.3

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -11,18 +11,35 @@ class Player {
     required this.age,
   });
 
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
   void sayHello() {
     print("Hello, $name");
   }
 }
 
 void main() {
-  var player = new Player(
+  var player1 = new Player.createBluePlayer(
     age: 10,
     name: "susan",
-    team: "red",
-    xp: 1919,
   );
 
-  player.sayHello();
+  var player2 = new Player.createRedPlayer(
+    age: 15,
+    name: "john",
+  );
 }
