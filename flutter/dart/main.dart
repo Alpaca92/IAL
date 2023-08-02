850c20e3b90f2008bb1e3d9362bd002cceefe61b
ayaan <ayaan@t-win.kr>
2023-08-02 15:13:13 +0900

    dart #4.8(2)

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -3,7 +3,9 @@ enum Team { blue, red }
 class Human {
   final String name;
 
-  Human(this.name);
+  Human({
+    required this.name,
+  });
 
   void sayHello() {
     print("Hello, $name");
@@ -15,8 +17,8 @@ class Player extends Human {
 
   Player({
     required this.team,
-    required String name,
-  }) : super(name);
+    required super.name,
+  });
 
   @override
   void sayHello() {
