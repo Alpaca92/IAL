1323c0624967499ac9cb55fdc5a8ce16e2944fd5
ayaan <zxcvbbnm5288@gmail.com>
2023-08-02 13:45:45 +0900

    dart #4.2

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,1 +1,28 @@
-void main() {}
+class Player {
+  final String name;
+  int xp;
+  String team;
+  int age;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  void sayHello() {
+    print("Hello, $name");
+  }
+}
+
+void main() {
+  var player = new Player(
+    age: 10,
+    name: "susan",
+    team: "red",
+    xp: 1919,
+  );
+
+  player.sayHello();
+}
