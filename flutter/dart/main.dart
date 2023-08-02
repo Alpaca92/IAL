49c17419b1e74609224af069b9cb4e2eb4bfecd4
ayaan <ayaan@t-win.kr>
2023-08-02 10:15:57 +0900

    dart #3.1

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,10 +1,22 @@
-void hello(String name) {
-  print("Hello, $name");
+String sayHello(String name, int age, String country) {
+  return "Hello $name, you are $age, and you live in $country";
 }
 
-String hello2(String name) => "Hello, $name";
+// named args
+String sayHello2({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return "Hello $name, you are $age, and you live in $country";
+}
 
 void main() {
-  hello('john');
-  print(hello2('susan'));
+  print(sayHello('susan', 19, 'Busan'));
+
+  print(sayHello2(
+    name: 'susan2',
+    country: 'Suwon',
+    age: 20,
+  ));
 }
