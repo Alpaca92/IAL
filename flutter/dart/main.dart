de3544b89d251e7b1019f096c278e80e3f0fc6f0
ayaan <be.minimal.o.o9@gmail.com>
2023-08-02 13:20:04 +0900

    test commit

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,16 +1,1 @@
-class Player {
-  final String name;
-  int xp;
-
-  // constructor
-  Player(this.name, this.xp);
-
-  void sayHello() {
-    print("Hello, my name is $name");
-  }
-}
-
-void main() {
-  var player = new Player('susan', 1500);
-  player.sayHello();
-}
+void main() {}
