49192d8f8a84e9e473e8c2231c882a1ff5fd8e02
ayaan <ayaan@t-win.kr>
2023-08-02 09:55:04 +0900

    dart #2.5

--- flutter/dart/main.dart
+++ flutter/dart/main.dart
@@ -1,9 +1,8 @@
 void main() {
-  var player = {
-    'name': 'John',
-    'xp': 19.11,
+  var numbers = {
+    1,
+    2,
+    3,
+    4,
   };
-
-  // Map<String, Object>인 이유는 type은 모두 Object에서 상속되기 때문이다
-  // typescript의 any에 해당한다고 보면된다
 }
