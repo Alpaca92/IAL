1eebc9bd94aa68d004a76a080eeabbd30d4bfa6f
ayaan <ayaan@t-win.kr>
2023-08-09 16:01:06 +0900

    flutter #6.1

--- flutter/toonflix/lib/screens/home_screen.dart
+++ flutter/toonflix/lib/screens/home_screen.dart
@@ -1,163 +1,24 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  bool isRunning = false;
-  int totalSeconds = twentyFiveMinutes;
-  int totalPomodoros = 0;
-  late Timer timer;
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        totalPomodoros += 1;
-        isRunning = false;
-        totalSeconds = twentyFiveMinutes;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds -= 1;
-      });
-    }
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(
-      const Duration(seconds: 1),
-      onTick,
-    );
-
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  void onResetPressed() {
-    timer.cancel();
-
-    setState(() {
-      isRunning = false;
-      totalSeconds = twentyFiveMinutes;
-    });
-  }
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-
-    return duration.toString().split('.').first.substring(2);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 90,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Today's webtoons",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w400,
           ),
-          Flexible(
-            flex: 3,
-            child: Center(
-              child: isRunning
-                  ? Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.pause_circle_filled_rounded),
-                          iconSize: 120,
-                          color: Theme.of(context).cardColor,
-                          onPressed: onPausePressed,
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.replay_circle_filled_outlined),
-                          iconSize: 120,
-                          color: Theme.of(context).cardColor,
-                          onPressed: onResetPressed,
-                        ),
-                      ],
-                    )
-                  : IconButton(
-                      icon: const Icon(Icons.play_circle_filled),
-                      iconSize: 120,
-                      color: Theme.of(context).cardColor,
-                      onPressed: onStartPressed,
-                    ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                      borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(20),
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pomodoros',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(context)
-                                .textTheme
-                                .headlineLarge!
-                                .color,
-                          ),
-                        ),
-                        Text(
-                          '$totalPomodoros',
-                          style: TextStyle(
-                            fontSize: 60,
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(context)
-                                .textTheme
-                                .headlineLarge!
-                                .color,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.greenAccent,
+        elevation: 1,
       ),
     );
   }
