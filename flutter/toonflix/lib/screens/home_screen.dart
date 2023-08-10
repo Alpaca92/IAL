f32f500da7522903405e18f8baab29be37699e59
ayaan <ayaan@t-win.kr>
2023-08-10 11:26:41 +0900

    flutter #6.9

--- flutter/toonflix/lib/screens/home_screen.dart
+++ flutter/toonflix/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
+import 'package:toonflix/widgets/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -55,31 +56,11 @@ class HomeScreen extends StatelessWidget {
       ),
       itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 15),
-                    color: Colors.black.withOpacity(0.2),
-                  )
-                ],
-              ),
-              width: 250,
-              child: Image.network(webtoon.thumbnail),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              webtoon.title,
-              style: const TextStyle(
-                fontSize: 22,
-              ),
-            ),
-          ],
+
+        return Webtoon(
+          title: webtoon.title,
+          thumbnail: webtoon.thumbnail,
+          id: webtoon.id,
         );
       },
       separatorBuilder: (context, index) => const SizedBox(width: 40),
