1633351c93d2d9e1c5f27431813371170b873b39
acapla <zxcvbnm5288@gmail.com>
2024-01-03 21:47:32 +0900

    #1.0 hello world

--- dart/main.dart
+++ dart/main.dart
@@ -1,26 +1,3 @@
-enum Team { blue, red }
-
-mixin class Strong {
-  final double strengthLevel = 15.9;
+void main() {
+  print('Hello world');
 }
-
-mixin class QuickRunner {
-  void runQuick() {
-    print("run quick !");
-  }
-}
-
-mixin class Tall {
-  final double height = 1.99;
-}
-
-// mixin: no constructor
-class Player with Tall, Strong, QuickRunner {
-  final Team team;
-
-  Player({
-    required this.team,
-  });
-}
-
-void main() {}
