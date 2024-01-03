fe8899f9ae72327e84cf197b38eeae3e86ea3961
acapla <zxcvbnm5288@gmail.com>
2024-01-03 22:05:16 +0900

    #1.1 The Var Keyword

--- dart/main.dart
+++ dart/main.dart
@@ -1,3 +1,13 @@
 void main() {
-  print('Hello world');
+  // 1. var는 타입 추론을 한다
+  var name = 'acapla';
+  name = 'some string';
+
+  print('my name is $name');
+
+  // 2. 명시적 타입
+  String new_name = 'acapla';
+  new_name = 'another string';
+
+  print('my new_name is $new_name');
 }
