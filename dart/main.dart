a1e5119bc6f3452b264d7af14fa5f69e77db8261
acapla <zxcvbnm5288@gmail.com>
2024-01-06 22:34:45 +0900

    #1.5 Late Variables

--- dart/main.dart
+++ dart/main.dart
@@ -1,4 +1,7 @@
 void main() {
-  final String name = 'final';
-  name = 'test';
+  late final String name;
+
+  // API 호출 등으로 인해 나중에 값이 할당 됨
+  name = 'api call';
+  name = 'final doesn\'t change';
 }
