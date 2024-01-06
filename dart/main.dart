41af68668e09a10d2fe03f69e59e0a31b28edf93
acapla <zxcvbnm5288@gmail.com>
2024-01-06 22:30:48 +0900

    #1.3 Nullable Variables

--- dart/main.dart
+++ dart/main.dart
@@ -1,12 +1,8 @@
 void main() {
-  // 3. dynamic type
-  // 어떤 타입으로 들어올지 모르는 경우 사용하면 유용함
-  var name;
-  name = '';
-  name = 1;
-  name = false;
+  String? name = 'test';
+  name = null;
 
-  if (name is String) {
-    // String에 대한 method를 사용할 수 있음
+  if (name != null) {
+    name.length; // null safety
   }
 }
