995501c19d705d31eff10208a790438e1cf5112a
acapla <zxcvbnm5288@gmail.com>
2024-01-07 17:00:14 +0900

    #2.5 Sets

--- dart/main.dart
+++ dart/main.dart
@@ -1,6 +1,15 @@
 void main() {
-  var player = {
-    'name': 'acapla',
-    'xp': 10.90,
-  }
+  var numbers = {1, 2, 3, 4, 5};
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+
+  print(numbers);
 }
