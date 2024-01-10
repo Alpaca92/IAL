56db604de60a7694808ef3fa39e50e5ff782746f
acapla <zxcvbnm5288@gmail.com>
2024-01-10 23:45:37 +0900

    #3.1 Named Parameters

--- dart/main.dart
+++ dart/main.dart
@@ -1,10 +1,16 @@
-void sayHello(String name) {
-  print('Hello $name');
+String sayHello(String name, int age, String country) {
+  return 'Hello $name, Are you $age years old and from $country ?';
 }
 
-String sayHello2(String name) => "Hello $name";
+String sayHello2({
+  required String name,
+  required String country,
+  int age = 31,
+}) {
+  return 'Hello $name, Are you $age years old and from $country ?';
+}
 
 void main() {
-  sayHello('acapla');
-  print(sayHello2('acapla'));
+  print(sayHello('acapla', 31, 'Korea'));
+  print(sayHello2(name: 'acapla', age: 11, country: 'Korea'));
 }
