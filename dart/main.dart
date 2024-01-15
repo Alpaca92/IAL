2a868d1e32b1d6f69ae77fd06852699141d6e855
ayaan <ayaan@t-win.kr>
2024-01-15 13:58:22 +0900

    #3.3 Optional Positional Parameters

--- dart/main.dart
+++ dart/main.dart
@@ -1,16 +1,5 @@
-String sayHello(String name, int age, String country) {
-  return 'Hello $name, Are you $age years old and from $country ?';
-}
-
-String sayHello2({
-  required String name,
-  required String country,
-  int age = 31,
-}) {
-  return 'Hello $name, Are you $age years old and from $country ?';
-}
-
+String sayHello(String name, int age, [String? country = 'Korea']) =>
+    'Hello $name, You are $age years old and You from $country';
 void main() {
-  print(sayHello('acapla', 31, 'Korea'));
-  print(sayHello2(name: 'acapla', age: 11, country: 'Korea'));
+  print(sayHello('acapla', 31));
 }
