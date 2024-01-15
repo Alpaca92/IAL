c3eec991e32371fba3aaaac1c31bc30578bd8fa8
ayaan <ayaan@t-win.kr>
2024-01-16 08:45:32 +0900

    #4.3 Named Constructors

--- dart/main.dart
+++ dart/main.dart
@@ -1,8 +1,7 @@
 class Player {
   final String name;
-  int xp;
   String team;
-  int age;
+  int age, xp;
 
   Player({
     required this.name,
@@ -11,18 +10,32 @@ class Player {
     required this.age,
   });
 
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  }) : this.age = age, this.name = name, this.team = 'blue', this.xp = 0;
+
+  Player.createRedPlayer({
+    required String name,
+    required int age,
+  }) : this.age = age, this.name = name, this.team = 'red', this.xp = 0;
+
   void sayHello() {
     print('Hi my name is $name'); // not recommended use this keyword
   }
 }
 
 void main() {
-  var player = Player(
-    name: 'acaplaaaa',
-    xp: 2000,
-    team: 'blue',
+  var bluePlayer = Player.createBluePlayer(
+    name: 'blue_acapla',
     age: 30,
   );
 
-  player.sayHello();
+  var redPlayer = Player.createRedPlayer(
+    name: 'red_acapla',
+    age: 31,
+  );
+
+  bluePlayer.sayHello();
+  redPlayer.sayHello();
 }
