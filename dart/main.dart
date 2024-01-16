6106f03334fea7a23d717e974a91173e4d6902e0
acapla <zxcvbnm5288@gmail.com>
2024-01-16 23:36:31 +0900

    #4.7 Abstract Classes

--- dart/main.dart
+++ dart/main.dart
@@ -1,6 +1,10 @@
+abstract class Human {
+  void walk();
+}
+
 enum Team { red, blue }
 
-class Player {
+class Player extends Human {
   String name;
   Team team;
   int xp;
@@ -11,6 +15,10 @@ class Player {
     required this.team,
   });
 
+  void walk() {
+    print('i am walking');
+  }
+
   void sayHello() {
     print(
         'Hi my name is $name, my team is ${team.toString()}'); // not recommended use this keyword
@@ -18,6 +26,10 @@ class Player {
 }
 
 void main() {
-  var player = Player(name: 'name', xp: 0, team: Team.red);
+  var player = Player(
+    name: 'name',
+    xp: 0,
+    team: Team.red,
+  );
   player.sayHello();
 }
