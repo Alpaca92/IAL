0182d4ecb4397241d2bafdaece5a6f44dae40095
acapla <zxcvbnm5288@gmail.com>
2024-01-23 09:50:53 +0900

    #5.0 User Interface

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,88 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatefulWidget {
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        textTheme: TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xfff4eddb),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : Text('nothing '),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: Icon(Icons.remove_red_eye_rounded),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    // build method보다 먼저 호출되야하며, 단 한 번만 호출 할 수 있음
-    super.initState();
-    print('init state');
-  }
-
-  @override
-  void dispose() {
-    // 위젯이 스크린에서 제거될 때 호출되는 method
-    super.dispose();
-    print('dispose method');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build method');
-
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
-      ),
-    );
-  }
-}
-
-// initState => build => dispose
