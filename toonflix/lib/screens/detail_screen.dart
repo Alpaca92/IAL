e7b6709c8316a9fffe4e9bf9e2cb8c47c0360039
ayaan <ayaan@t-win.kr>
2024-01-26 13:51:06 +0900

    #6.10 Hero

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -35,25 +35,28 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 7,
-                      offset: Offset(5, 5),
-                      color: Colors.black.withOpacity(0.3),
-                    ),
-                  ],
-                ),
-                width: 250,
-                child: Image.network(
-                  thumb,
-                  headers: const {
-                    "User-Agent":
-                        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
-                  },
+              Hero(
+                tag: id,
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 7,
+                        offset: Offset(5, 5),
+                        color: Colors.black.withOpacity(0.3),
+                      ),
+                    ],
+                  ),
+                  width: 250,
+                  child: Image.network(
+                    thumb,
+                    headers: const {
+                      "User-Agent":
+                          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+                    },
+                  ),
                 ),
               ),
             ],
