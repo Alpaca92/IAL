0a6c3b6a45e4150f625db407dc7537b5f0eb9cd5
ayaan <ayaan@t-win.kr>
2024-02-06 17:06:11 +0900

    #6.5 Custom NavigationBar

--- tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -10,6 +11,8 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
   final screens = [
     const Center(
       child: Text('Home'),
@@ -19,26 +22,47 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     ),
   ];
 
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.house,
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            NavTab(
+              text: 'Home',
+              isSelected: _selectedIndex == 0,
+              icon: FontAwesomeIcons.house,
+              onTap: () => _onTap(0),
             ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.search,
+            NavTab(
+              text: 'Discover',
+              isSelected: _selectedIndex == 1,
+              icon: FontAwesomeIcons.magnifyingGlass,
+              onTap: () => _onTap(1),
             ),
-            label: 'Searh',
-          ),
-        ],
+            NavTab(
+              text: 'Inbox',
+              isSelected: _selectedIndex == 3,
+              icon: FontAwesomeIcons.message,
+              onTap: () => _onTap(3),
+            ),
+            NavTab(
+              text: 'Profile',
+              isSelected: _selectedIndex == 4,
+              icon: FontAwesomeIcons.user,
+              onTap: () => _onTap(4),
+            ),
+          ],
+        ),
       ),
-      tabBuilder: (context, index) => screens[index],
     );
   }
 }
