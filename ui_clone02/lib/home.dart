375c9df278f9c611cc7585e1ab48fb6d7fbf1116
ayaan <ayaan@t-win.kr>
2024-02-16 15:03:24 +0900

    clone pomodoros (1)

--- ui_clone02/lib/home.dart
+++ ui_clone02/lib/home.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static const List<int> timerList = [900, 1200, 1500, 1800, 2100];
+  int selectedIndex = 2;
+  late int remainingTime;
+
+  @override
+  void initState() {
+    super.initState();
+    remainingTime = timerList[selectedIndex];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +31,37 @@ class Home extends StatelessWidget {
           ),
         ),
         title: const Text('POMOTIMER'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 20,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: const Text(
+                      '12',
+                      style: TextStyle(
+                        color: Color(0xffe64d3d),
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
