0d602d4673fe01d7bfb5e226b2422e0b3ed6ca1c
Alpaca92 <zxcvbnm5288@gmail.com>
2024-08-29 12:25:41 +0900

    create routes, controller

--- api/app.js
+++ api/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import authRoute from './routes/auth.route.js';
+import postRoute from './routes/post.route.js';
 
 const app = express();
 
-console.log('Test');
+app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
 
 app.listen(8800, () => {
   console.log('Server is running on port 8800');
