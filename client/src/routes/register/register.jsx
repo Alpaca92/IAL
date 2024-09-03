df01bfa76a182bfd679eceae641dafd0db48407d
acapla <zxcvbnm5288@gmail.com>
2024-09-03 21:48:23 +0900

    1:03:00

--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,11 +1,37 @@
-import './register.scss';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import './register.scss';
+import { useState } from 'react';
 
 function Register() {
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    const formData = new FormData(event.target);
+
+    const username = formData.get('username');
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+      const res = await axios.post('http://localhost:8800/api/auth/register', {
+        username,
+        email,
+        password,
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
