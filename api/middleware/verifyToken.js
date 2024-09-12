c13cf811713736b08648a0098339728fee25195b
acapla <zxcvbnm5288@gmail.com>
2024-09-12 20:27:30 +0900

    1:30:20

---
+++ api/middleware/verifyToken.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Not Authenticated' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid Token' });
+    }
+
+    req.userId = payload.id;
+    next();
+  });
+
+  return res.status(200).json({ message: 'Authorized' });
+};
