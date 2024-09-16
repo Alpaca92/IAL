f9492bd5ded8e0d4fb05f378222c85820c2313f9
acapla <zxcvbnm5288@gmail.com>
2024-09-16 16:47:32 +0900

    1:41:00

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import './login.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import apiRequest from '../../lib/apiRequest';
+import { AuthContext } from '../../context/AuthContext';
 
 function Login() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const { updateUser } = useContext(AuthContext);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -22,8 +24,7 @@ function Login() {
         password,
       });
 
-      localStorage.setItem('user', JSON.stringify(res.data));
-
+      updateUser(res.data);
       navigate('/');
     } catch (error) {
       console.log(error);
