d0011f2414aba6e88293f8275a419ee1e9f5ad8b
acapla <zxcvbnm5288@gmail.com>
2024-09-17 17:43:18 +0900

    1:50:40

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,35 @@
+export const getUsers = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'failed to get users' });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'failed to get user' });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'failed to update user' });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    //
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'failed to delete user' });
+  }
+};
--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -2,9 +2,9 @@ import Chat from '../../components/chat/Chat';
 import List from '../../components/list/List';
 import './profilePage.scss';
 import apiRequest from '../../lib/apiRequest';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 
 function ProfilePage() {
   const { currentUser, updateUser } = useContext(AuthContext);
@@ -21,52 +21,46 @@ function ProfilePage() {
     }
   };
 
-  useEffect(() => {
-    if (!currentUser) {
-      navigate('/login');
-    }
-  }, [currentUser]);
-
   return (
-    currentUser && (
-      <div className="profilePage">
-        <div className="details">
-          <div className="wrapper">
-            <div className="title">
-              <h1>User Information</h1>
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <Link to="/profile/update">
               <button>Update Profile</button>
-            </div>
-            <div className="info">
-              <span>
-                Avatar:
-                <img src={currentUser.avatar || '/noavatar.png'} alt="" />
-              </span>
-              <span>
-                Username: <b>{currentUser.username}</b>
-              </span>
-              <span>
-                E-mail: <b>{currentUser.email}</b>
-              </span>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-            <div className="title">
-              <h1>My List</h1>
-              <button>Create New Post</button>
-            </div>
-            <List />
-            <div className="title">
-              <h1>Saved List</h1>
-            </div>
-            <List />
+            </Link>
           </div>
-        </div>
-        <div className="chatContainer">
-          <div className="wrapper">
-            <Chat />
+          <div className="info">
+            <span>
+              Avatar:
+              <img src={currentUser?.avatar || '/noavatar.png'} alt="" />
+            </span>
+            <span>
+              Username: <b>{currentUser?.username}</b>
+            </span>
+            <span>
+              E-mail: <b>{currentUser?.email}</b>
+            </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
         </div>
       </div>
-    )
+      <div className="chatContainer">
+        <div className="wrapper">
+          <Chat />
+        </div>
+      </div>
+    </div>
   );
 }
 
