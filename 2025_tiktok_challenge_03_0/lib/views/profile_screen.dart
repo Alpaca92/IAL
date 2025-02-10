0b8015a016ad082e909e98c25ce0c20b53f81c05
ayaan <zxcvbnm5288@gmail.com>
2025-02-10 22:40:00 +0900

    feat: #16 | Profile and Settings
    - add SliverToBoxAdapter

--- 2025_tiktok_challenge_03_0/lib/views/profile_screen.dart
+++ 2025_tiktok_challenge_03_0/lib/views/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_challenge_04/constants/gaps.dart';
+import 'package:tiktok_challenge_04/constants/sizes.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({
@@ -28,7 +30,151 @@ class ProfileScreen extends StatelessWidget {
             ),
           ],
         ),
-        SliverToBoxAdapter(),
+        SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: Sizes.size16),
+          sliver: SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v24,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ayaan',
+                          style: TextStyle(
+                            fontSize: Sizes.size24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Gaps.v8,
+                        Row(
+                          children: [
+                            Text('be.minimal.o.o9'),
+                            Gaps.h8,
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: Sizes.size10,
+                                vertical: Sizes.size6,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade200,
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                              child: Text(
+                                'threads.net',
+                                style: TextStyle(
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.teal.shade100,
+                      child: Text(
+                        '🦙',
+                        style: TextStyle(
+                          fontSize: Sizes.size28,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v16,
+                Text(
+                  'Palindromic Programmer 🧑‍💻',
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Gaps.v20,
+                Row(
+                  children: [
+                    Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Colors.orange.shade100,
+                          child: Text('🥇'),
+                        ),
+                        Positioned(
+                          left: 24,
+                          child: CircleAvatar(
+                            radius: 16,
+                            backgroundColor: Colors.amber.shade100,
+                            child: Text('🥈'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Gaps.h40,
+                    Text(
+                      '2 followers',
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v32,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: Sizes.size12),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        child: Text(
+                          'Edit profile',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: Sizes.size16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Gaps.h16,
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: Sizes.size12),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        child: Text(
+                          'Share profile',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: Sizes.size16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
