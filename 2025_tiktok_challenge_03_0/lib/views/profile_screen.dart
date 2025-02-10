50743c5a9a2b97f989b58db0aae37d3895a059d7
ayaan <zxcvbnm5288@gmail.com>
2025-02-10 22:04:54 +0900

    feat: #16: Profile and Settings
    - add SliverAppBar

--- 2025_tiktok_challenge_03_0/lib/views/profile_screen.dart
+++ 2025_tiktok_challenge_03_0/lib/views/profile_screen.dart
@@ -1,13 +1,35 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
-  static const String routeURL = '/profile';
-  static const String routeName = 'profile';
+  const ProfileScreen({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('profile'),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          leading: IconButton(
+            icon: FaIcon(FontAwesomeIcons.globe),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              icon: FaIcon(FontAwesomeIcons.instagram),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: FaIcon(FontAwesomeIcons.sliders),
+              onPressed: () {
+                // TODO: push to settings screen
+              },
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(),
+      ],
     );
   }
 }
