1c6eededd004d41343e4be05de2d5922931e3ea4
ayaan <zxcvbnm5288@gmail.com>
2025-02-11 22:30:36 +0900

    feat: add padding

--- 2025_tiktok_challenge_03_0/lib/widgets/persistent_tab_bar.dart
+++ 2025_tiktok_challenge_03_0/lib/widgets/persistent_tab_bar.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_challenge_04/constants/sizes.dart';
 
 class PersistentTabBar extends SliverPersistentHeaderDelegate {
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 0.5,
+          ),
+        ),
+      ),
       child: TabBar(
         tabs: [
-          Text('Threads'),
-          Text('Replies'),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: Sizes.size8),
+            child: Text(
+              'Threads',
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: Sizes.size8),
+            child: Text(
+              'Replies',
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
   @override
-  double get maxExtent => 20;
+  double get maxExtent => 40;
 
   @override
-  double get minExtent => 20;
+  double get minExtent => 40;
 
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
