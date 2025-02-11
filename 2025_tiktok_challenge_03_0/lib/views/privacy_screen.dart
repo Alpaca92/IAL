1d5fee7867a1f97def29ad9466c1b5d1b07e9f33
ayaan <zxcvbnm5288@gmail.com>
2025-02-11 21:55:25 +0900

    feat: privacy screen switch list tile widget

---
+++ 2025_tiktok_challenge_03_0/lib/views/privacy_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_challenge_04/constants/gaps.dart';
+import 'package:tiktok_challenge_04/constants/sizes.dart';
+
+class PrivacyScreen extends StatefulWidget {
+  const PrivacyScreen({super.key});
+
+  @override
+  State<PrivacyScreen> createState() => _PrivacyScreenState();
+}
+
+class _PrivacyScreenState extends State<PrivacyScreen> {
+  bool _switchValue = true;
+
+  void _onChanged(bool value) {
+    setState(() {
+      _switchValue = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Privacy'),
+        centerTitle: true,
+        shape: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: Sizes.size1 / 2,
+          ),
+        ),
+        leadingWidth: Sizes.size80,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Row(
+            children: [
+              Gaps.h12,
+              FaIcon(FontAwesomeIcons.chevronLeft),
+              Gaps.h6,
+              Text(
+                'Back',
+                style: TextStyle(
+                  fontSize: Sizes.size18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _switchValue,
+            onChanged: _onChanged,
+            activeTrackColor: Colors.black,
+            inactiveTrackColor: Colors.white,
+            inactiveThumbColor: Colors.grey[300],
+            title: Row(
+              children: [
+                FaIcon(FontAwesomeIcons.lock),
+                Gaps.h12,
+                Text('Private profile'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- 2025_tiktok_challenge_03_0/lib/views/settings_screen.dart
+++ 2025_tiktok_challenge_03_0/lib/views/settings_screen.dart
@@ -1,10 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_challenge_04/constants/gaps.dart';
+import 'package:tiktok_challenge_04/constants/sizes.dart';
+import 'package:tiktok_challenge_04/views/privacy_screen.dart';
+
+typedef IconText = Map<String, dynamic>;
+
+const List<IconText> _iconTexts = [
+  {
+    'icon': FontAwesomeIcons.userPlus,
+    'text': 'Follow and invite friends',
+    'screen': Placeholder(),
+  },
+  {
+    'icon': FontAwesomeIcons.bell,
+    'text': 'Notifications',
+    'screen': Placeholder(),
+  },
+  {
+    'icon': FontAwesomeIcons.lock,
+    'text': 'Privacy',
+    'screen': PrivacyScreen(),
+  },
+  {
+    'icon': FontAwesomeIcons.circleUser,
+    'text': 'Account',
+    'screen': Placeholder(),
+  },
+  {
+    'icon': FontAwesomeIcons.lifeRing,
+    'text': 'Help',
+    'screen': Placeholder(),
+  },
+  {
+    'icon': FontAwesomeIcons.circleInfo,
+    'text': 'About',
+    'screen': Placeholder(),
+  },
+];
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+        centerTitle: true,
+        shape: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: Sizes.size1 / 2,
+          ),
+        ),
+        leadingWidth: Sizes.size80,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Row(
+            children: [
+              Gaps.h12,
+              FaIcon(FontAwesomeIcons.chevronLeft),
+              Gaps.h6,
+              Text(
+                'Back',
+                style: TextStyle(
+                  fontSize: Sizes.size18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Gaps.v24,
+          ..._iconTexts.map(
+            (iconText) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => iconText['screen'],
+                    ),
+                  );
+                },
+                child: ListTile(
+                  leading: FaIcon(iconText['icon']),
+                  title: Text(iconText['text']),
+                  trailing: FaIcon(FontAwesomeIcons.chevronRight),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
