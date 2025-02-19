749fdc08d8e2dce870495690f8774f4daea86867
ayaan <zxcvbnm5288@gmail.com>
2025-02-20 05:05:53 +0900

    feat: #5.2

--- tiktok_clone/lib/features/onboarding/interests_screen.dart
+++ tiktok_clone/lib/features/onboarding/interests_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/widgets/interest_button.dart';
 
 const interests = [
   "Daily Life",
@@ -43,75 +44,74 @@ const interests = [
   "Home & Garden",
 ];
 
-class InterestsScreen extends StatelessWidget {
+class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
+
+  @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  final ScrollController _scrollController = ScrollController();
+  bool _showTitle = false;
+
+  void _onScroll() {
+    setState(() {
+      bool over100 = _scrollController.offset > 100;
+
+      if (over100 && _showTitle) return;
+      _showTitle = over100 ? true : false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      _onScroll();
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Choose your interests'),
+        title: AnimatedOpacity(
+          opacity: _showTitle ? 1 : 0,
+          duration: const Duration(milliseconds: 300),
+          child: const Text('Choose your interests'),
+        ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: Sizes.size24,
-            right: Sizes.size24,
-            bottom: Sizes.size20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Gaps.v32,
-              const Text(
-                'Choose your interests',
-                style: TextStyle(
-                  fontSize: Sizes.size44,
-                  fontWeight: FontWeight.bold,
+      body: Scrollbar(
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Padding(
+            padding: const EdgeInsets.only(left: Sizes.size24, right: Sizes.size24, bottom: Sizes.size20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v32,
+                const Text(
+                  'Choose your interests',
+                  style: TextStyle(fontSize: Sizes.size44, fontWeight: FontWeight.bold),
                 ),
-              ),
-              Gaps.v20,
-              const Text(
-                'Get better vidio recommendations',
-                style: TextStyle(
-                  fontSize: Sizes.size20,
+                Gaps.v20,
+                const Text('Get better vidio recommendations', style: TextStyle(fontSize: Sizes.size20)),
+                Gaps.v64,
+                Wrap(
+                  spacing: Sizes.size12,
+                  runSpacing: Sizes.size20,
+                  children: [for (var interest in interests) InterestButton(interest: interest)],
                 ),
-              ),
-              Gaps.v64,
-              Wrap(
-                spacing: Sizes.size12,
-                runSpacing: Sizes.size20,
-                children: [
-                  for (var interest in interests)
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: Sizes.size16,
-                        horizontal: Sizes.size24,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(Sizes.size32),
-                        border: Border.all(
-                          color: Colors.black.withValues(alpha: 0.1),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withValues(alpha: 0.1),
-                            blurRadius: 5,
-                            spreadRadius: 2,
-                          ),
-                        ],
-                      ),
-                      child: Text(
-                        interest,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -121,18 +121,10 @@ class InterestsScreen extends StatelessWidget {
         height: 100,
         child: Container(
           margin: const EdgeInsets.only(bottom: 20),
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-          ),
+          decoration: BoxDecoration(color: Theme.of(context).primaryColor),
           child: const Align(
             alignment: Alignment.center,
-            child: Text(
-              'Next',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: Sizes.size16,
-              ),
-            ),
+            child: Text('Next', style: TextStyle(color: Colors.white, fontSize: Sizes.size16)),
           ),
         ),
       ),
--- tiktok_clone/lib/features/videos/widgets/video_comments.dart
+++ tiktok_clone/lib/features/videos/widgets/video_comments.dart
@@ -189,7 +189,7 @@ class _VideoCommentsState extends State<VideoComments> {
                                           ),
                                         ),
                                     ],
-                                  )),
+                                  ),),
                             ),
                           ),
                         ),
