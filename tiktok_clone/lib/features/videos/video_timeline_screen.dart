6f620b79d6454caa68f294dbe565b1a4df629177
ayaan <zxcvbnm5288@gmail.com>
2025-02-26 22:13:19 +0900

    feat: #7.3 (2)

--- tiktok_clone/lib/features/videos/video_timeline_screen.dart
+++ tiktok_clone/lib/features/videos/video_timeline_screen.dart
@@ -9,14 +9,16 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
-  int _itemCount = 4;
   final PageController _pageController = PageController();
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
+  int _itemCount = 4;
 
   void _onPageChanged(int page) {
     _pageController.animateToPage(
       page,
-      duration: const Duration(milliseconds: 100),
-      curve: Curves.linear,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
     );
 
     if (page == _itemCount - 1) {
@@ -26,6 +28,16 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     }
   }
 
+  void _onVideoFinished() {
+    _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
@@ -33,7 +45,8 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => const VideoPost(),
+      itemBuilder:
+          (context, index) => VideoPost(onVideoFinished: _onVideoFinished),
     );
   }
 }
--- tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPost extends StatefulWidget {
-  const VideoPost({super.key});
+  final Function onVideoFinished;
+
+  const VideoPost({super.key, required this.onVideoFinished});
 
   @override
   State<VideoPost> createState() => _VideoPostState();
@@ -10,18 +12,34 @@ class VideoPost extends StatefulWidget {
 
 class _VideoPostState extends State<VideoPost> {
   final VideoPlayerController _videoPlayerController =
-      VideoPlayerController.asset('assets/portrait_example01.mp4');
+      VideoPlayerController.asset('assets/videos/portrait_example01.mp4');
+
+  void _onVideoChange() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
+        widget.onVideoFinished();
+      }
+    }
+  }
 
   void _initializeVideoPlayer() async {
     await _videoPlayerController.initialize();
     _videoPlayerController.play();
     setState(() {});
+    _videoPlayerController.addListener(_onVideoChange);
   }
 
   @override
   void initState() {
     super.initState();
     _initializeVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
   }
 
   @override
