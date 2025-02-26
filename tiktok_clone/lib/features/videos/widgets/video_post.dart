6679244c502da1e593c0a63c7f54065ae9753253
ayaan <zxcvbnm5288@gmail.com>
2025-02-26 16:11:44 +0900

    feat: #7.2

--- tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/features/videos/widgets/video_comments.dart';
 
-// NOTE: 진도가 밀려서 건너뛴 부분이 있음
-// #5.2 ~#7.9
-// 위 부분은 조금씩 학습할 것
-class VideoPostScreen extends StatelessWidget {
-  const VideoPostScreen({super.key});
+class VideoPost extends StatefulWidget {
+  const VideoPost({super.key});
 
+  @override
+  State<VideoPost> createState() => _VideoPostState();
+}
+
+class _VideoPostState extends State<VideoPost> {
   void _onCommentsTap(BuildContext context) async {
     // if (_videoPlayerController.value.isPlaying) {
     //   _onTogglePause();
