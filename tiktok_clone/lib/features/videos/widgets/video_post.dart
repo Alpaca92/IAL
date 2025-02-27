019414ee2af9ef6fc3d746b0b9e251833bb95848
ayaan <zxcvbnm5288@gmail.com>
2025-02-27 22:57:34 +0900

    feat: #7.4

--- tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
+  final int index;
   final Function onVideoFinished;
 
-  const VideoPost({super.key, required this.onVideoFinished});
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+    required this.index,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
@@ -15,9 +23,15 @@ class _VideoPostState extends State<VideoPost> {
       VideoPlayerController.asset('assets/videos/portrait_example01.mp4');
 
   void _onVideoChange() {
-    if (_videoPlayerController.value.isInitialized) {
-      if (_videoPlayerController.value.duration ==
-          _videoPlayerController.value.position) {
+    // NOTE: The following code is a pattern matching syntax.
+    //       It is a syntax that matches the type of the object and assigns it to a variable.
+    //       It is a syntax that is often used in the Dart language.
+    final VideoPlayerController(
+      value: VideoPlayerValue(:isInitialized, :duration, :position),
+    ) = _videoPlayerController;
+
+    if (isInitialized) {
+      if (duration == position) {
         widget.onVideoFinished();
       }
     }
@@ -25,9 +39,28 @@ class _VideoPostState extends State<VideoPost> {
 
   void _initializeVideoPlayer() async {
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
     setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
+  }
+
+  void _onVisibilityChanged(VisibilityInfo info) {
+    final VisibilityInfo(:visibleFraction) = info;
+    final VideoPlayerController(:play, value: VideoPlayerValue(:isPlaying)) =
+        _videoPlayerController;
+
+    if (visibleFraction == 1 && !isPlaying) {
+      play();
+    }
+  }
+
+  void _onTogglePause() {
+    final VideoPlayerController(
+      :play,
+      :pause,
+      value: VideoPlayerValue(:isPlaying),
+    ) = _videoPlayerController;
+
+    isPlaying ? pause() : play();
   }
 
   @override
@@ -44,12 +77,28 @@ class _VideoPostState extends State<VideoPost> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        _videoPlayerController.value.isInitialized
-            ? VideoPlayer(_videoPlayerController)
-            : Positioned.fill(child: Container(color: Colors.black)),
-      ],
+    return VisibilityDetector(
+      key: Key('${widget.index}'),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          _videoPlayerController.value.isInitialized
+              ? VideoPlayer(_videoPlayerController)
+              : Positioned.fill(child: Container(color: Colors.black)),
+          Positioned.fill(child: GestureDetector(onTap: _onTogglePause)),
+          const Positioned.fill(
+            child: IgnorePointer(
+              child: Center(
+                child: FaIcon(
+                  FontAwesomeIcons.play,
+                  color: Colors.white,
+                  size: Sizes.size52,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
