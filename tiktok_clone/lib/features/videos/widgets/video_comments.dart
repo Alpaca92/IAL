a99ff49fe746a00290fe4a00db051e85bbdc2668
ayaan <zxcvbnm5288@gmail.com>
2025-03-02 08:45:15 +0900

    feat: #8.1

--- tiktok_clone/lib/features/videos/widgets/video_comments.dart
+++ tiktok_clone/lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -32,11 +33,69 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 10,
           itemBuilder: (context, index) {
-            return const ListTile(title: Text('Comment'));
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(radius: 18, child: Text('nico')),
+                Gaps.h10,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'nico',
+                        style: TextStyle(
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Gaps.v3,
+                      const Text(
+                        'That is not good! That is not good! That is not good! That is not good!',
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.h10,
+                Column(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                      size: Sizes.size20,
+                      color: Colors.grey.shade500,
+                    ),
+                    Gaps.v2,
+                    Text(
+                      '52.2K',
+                      style: TextStyle(color: Colors.grey.shade500),
+                    ),
+                  ],
+                ),
+              ],
+            );
           },
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade400,
+                foregroundColor: Colors.white,
+                child: const Text('nico'),
+              ),
+            ],
+          ),
         ),
       ),
     );
