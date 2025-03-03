b6dd0567617cb86c6237d5b6dbc2707e5fc59e30
ayaan <zxcvbnm5288@gmail.com>
2025-03-03 13:37:06 +0900

    feat: #9.4

--- tiktok_clone/lib/features/discover/discover_screen.dart
+++ tiktok_clone/lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 final tabs = ['Top', 'Users', 'Videos', 'LIVE', 'Sounds', 'Shpping', 'Brands'];
@@ -33,13 +35,64 @@ class DiscoverScreen extends StatelessWidget {
               padding: const EdgeInsets.all(Sizes.size6),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / 21,
                 mainAxisSpacing: Sizes.size10,
                 crossAxisSpacing: Sizes.size10,
               ),
               itemBuilder:
-                  (context, index) =>
-                      Container(color: Colors.teal, child: Text('$index')),
+                  (context, index) => Column(
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholder: 'assets/images/placeholder.jpg',
+                          image: 'https://source.unsplash.com/random/?$index',
+                        ),
+                      ),
+                      Gaps.v10,
+                      const Text(
+                        'This is a very long caption for my tiktok that i am upload just now currently.',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: Sizes.size16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Gaps.v5,
+                      DefaultTextStyle(
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        child: Row(
+                          children: [
+                            const CircleAvatar(
+                              radius: Sizes.size16,
+                              child: Text('name', textAlign: TextAlign.center),
+                            ),
+                            Gaps.h4,
+                            const Expanded(
+                              child: Text(
+                                'My avatar is going to be very long.',
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Gaps.h4,
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size16,
+                              color: Colors.grey.shade600,
+                            ),
+                            Gaps.h4,
+                            const Text('2.5M'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
             ),
             for (var tab in tabs.skip(1)) Center(child: Text(tab)),
           ],
