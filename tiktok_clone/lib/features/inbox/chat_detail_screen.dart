9d39d58a90cd8bd8073a1aa5046af89ec623dfed
ayaan <zxcvbnm5288@gmail.com>
2025-03-08 13:25:33 +0900

    feat: #11.4

--- tiktok_clone/lib/features/inbox/chat_detail_screen.dart
+++ tiktok_clone/lib/features/inbox/chat_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   const ChatDetailScreen({super.key});
@@ -10,6 +13,98 @@ class ChatDetailScreen extends StatefulWidget {
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: Sizes.size8,
+          leading: CircleAvatar(
+            backgroundColor: Colors.black,
+            child: Text('nico', style: TextStyle(color: Colors.white)),
+          ),
+          title: Text('nioco', style: TextStyle(fontWeight: FontWeight.w600)),
+          subtitle: Text('active now'),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h32,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          ListView.separated(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size20,
+              horizontal: Sizes.size14,
+            ),
+            itemCount: 10,
+            separatorBuilder: (context, index) => Gaps.v10,
+            itemBuilder: (context, index) {
+              final isMine = index % 2 == 0;
+
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment:
+                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(Sizes.size14),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(Sizes.size20),
+                        topRight: const Radius.circular(Sizes.size20),
+                        bottomLeft:
+                            isMine
+                                ? const Radius.circular(Sizes.size20)
+                                : const Radius.circular(Sizes.size5),
+                        bottomRight:
+                            isMine
+                                ? const Radius.circular(Sizes.size5)
+                                : const Radius.circular(Sizes.size20),
+                      ),
+                      color:
+                          isMine ? Colors.blue : Theme.of(context).primaryColor,
+                    ),
+                    child: const Text(
+                      'This is a message !',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            child: BottomAppBar(
+              color: Colors.grey.shade50,
+              child: Row(
+                children: [
+                  const Expanded(child: TextField()),
+                  Gaps.h20,
+                  Container(child: const FaIcon(FontAwesomeIcons.paperPlane)),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
